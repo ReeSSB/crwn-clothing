12ca5052ea9c39b411553e4c8dcb79265187a374
ReeSSB <shashi.as05@gmail.com>
2022-02-17 14:51:22 +0530

    added header page with shop, contact links along with logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import HomePage from "./pages/homepage/homepage.component";
 import { Routes, Route } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component.jsx";
+import Header from "./components/header/header.component";
 import "./App.css";
 
 function App() {
 	return (
 		<div>
+			<Header />
 			<Routes>
 				<Route path="/" element={<HomePage />} />
 				<Route path="/shop" element={<ShopPage />} />
