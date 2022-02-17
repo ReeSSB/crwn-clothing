13c04068351c64a21075eb91583a03dd8c976ebb
ReeSSB <shashi.as05@gmail.com>
2022-02-17 11:15:12 +0530

    update our menuitem to navigate

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import HomePage from "./pages/homepage/homepage.component";
+import { Routes, Route, useParams } from "react-router-dom";
 import "./App.css";
+
+const HatsPage = () => (
+	<div>
+		<h1>Hats Page</h1>
+	</div>
+);
+
+const TopicPage = (props) => {
+	console.log(props);
+	let params = useParams();
+	return (
+		<div>
+			<h1>TOPIC PAGE DETAILS:{params.topicId} </h1>
+		</div>
+	);
+};
+
 function App() {
 	return (
 		<div>
-			<HomePage />
+			<Routes>
+				<Route exact path="/" element={<HomePage />} />
+				<Route path="/shop/hats" element={<HatsPage />} />
+				<Route path="/topic/:topicId" element={<TopicPage />} />
+			</Routes>
 		</div>
 	);
 }
